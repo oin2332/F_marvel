7e9e533bef924db061c094b870ffa196979c1ba6
Son <smo3072@naver.com>
2023-10-30 16:30:24 +0900

    /1030/son

--- lib/map/googlemap.dart
+++ lib/map/googlemap.dart
@@ -5,132 +5,139 @@ import 'package:geolocator/geolocator.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
- const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
- @override
- _MyAppState createState() => _MyAppState();
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
 class Place {
- final String name;
- final String address;
- final LatLng location;
+  final String name;
+  final String address;
+  final LatLng location;
 
- Place({required this.name, required this.address, required this.location});
+  Place({required this.name, required this.address, required this.location});
 }
 
 List<Place> places = [
- Place(name: '칸다소바', address: '인천 부평구 부평대로36번길 5', location: LatLng(37.4910133, 126.7206483)),
- Place(name: '인브스키친', address: '인천 부평구 부평대로 39-6', location: LatLng(37.490815, 126.720895)),
- Place(name: '에픽', address: '인천 부평구 경원대로1363번길 8', location: LatLng(37.490539, 126.718667)),
+  Place(
+      name: '칸다소바',
+      address: '인천 부평구 부평대로36번길 5',
+      location: LatLng(37.4910133, 126.7206483)),
+  Place(
+      name: '인브스키친',
+      address: '인천 부평구 부평대로 39-6',
+      location: LatLng(37.490815, 126.720895)),
+  Place(
+      name: '에픽',
+      address: '인천 부평구 경원대로1363번길 8',
+      location: LatLng(37.490539, 126.718667)),
 ];
 
 class _MyAppState extends State<MyApp> {
- late GoogleMapController mapController;
- late LatLng _center = LatLng(37.4895, 126.7220); // 인천 부평구의 중심 좌표
+  late GoogleMapController mapController;
+  late LatLng _center = LatLng(37.4895, 126.7220); // 인천 부평구의 중심 좌표
 
- @override
- void initState() {
-  super.initState();
-  _getCurrentLocation(); // Get current location when the app starts
- }
-
- void _onMapCreated(GoogleMapController controller) {
-  mapController = controller;
- }
-
- Set<Marker> _markers = {};
-
- Future<void> _getCurrentLocation() async {
-  // Clear previous markers
-  _markers.clear();
-
-  // TODO: 위치 서비스를 사용하여 정확한 현재 위치 가져오기
-  Position position = await Geolocator.getCurrentPosition(
-   desiredAccuracy: LocationAccuracy.high,
-  );
-
-  // Move the camera to the current position
-  mapController.animateCamera(
-   CameraUpdate.newCameraPosition(
-    CameraPosition(
-     target: LatLng(position.latitude, position.longitude),
-     zoom: 15.0,
-    ),
-   ),
-  );
-
-  // Add markers for the current position
-  _markers.add(
-   Marker(
-    markerId: MarkerId('current_location'),
-    position: LatLng(position.latitude, position.longitude),
-    infoWindow: InfoWindow(
-     title: 'Current Location',
-     snippet: 'Lat: ${position.latitude}, Lng: ${position.longitude}',
-    ),
-   ),
-  );
-  // Add markers for the dummy places
-
-
-  // Add markers for the dummy places
-  void _addDummyMarkers() {
-   for (var place in places) {
-    _markers.add(
-     Marker(
-      markerId: MarkerId(place.name),
-      position: place.location,
-      infoWindow: InfoWindow(
-       title: place.name,
-       snippet: place.address,
-      ),
-     ),
-    );
-   }
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation(); // Get current location when the app starts
   }
-  // Add markers for the dummy places
-  _addDummyMarkers();
 
-  setState(() {}); // Rebuild the widget after updating markers
- }
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
- @override
- Widget build(BuildContext context) {
-  return MaterialApp(
-   home: Scaffold(
-    appBar: AppBar(
-     title: const Text('Maps Sample App'),
-     backgroundColor: Colors.green[700],
-    ),
-    body: Column(
-     children: <Widget>[
-      Expanded(
-       child: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: _center == null
-            ? const CameraPosition(
-         target: LatLng(37.4910133, 126.7206483),
-         zoom: 11.0,
-        )
-            : CameraPosition(
-         target: _center,
-         zoom: 15.0,
+  Set<Marker> _markers = {};
+
+  Future<void> _getCurrentLocation() async {
+    // Clear previous markers
+    _markers.clear();
+
+    // TODO: 위치 서비스를 사용하여 정확한 현재 위치 가져오기
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    // Move the camera to the current position
+    mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 15.0,
         ),
-        markers: _markers,
-       ),
       ),
-     ],
-    ),
-    floatingActionButton: FloatingActionButton(
-     onPressed: _getCurrentLocation,
-     child: Icon(Icons.my_location),
-     backgroundColor: Colors.blue[700],
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-   ),
-  );
- }
+    );
+
+    // Add markers for the current position
+    _markers.add(
+      Marker(
+        markerId: MarkerId('current_location'),
+        position: LatLng(position.latitude, position.longitude),
+        infoWindow: InfoWindow(
+          title: 'Current Location',
+          snippet: 'Lat: ${position.latitude}, Lng: ${position.longitude}',
+        ),
+      ),
+    );
+    // Add markers for the dummy places
+
+    // Add markers for the dummy places
+    void _addDummyMarkers() {
+      for (var place in places) {
+        _markers.add(
+          Marker(
+            markerId: MarkerId(place.name),
+            position: place.location,
+            infoWindow: InfoWindow(
+              title: place.name,
+              snippet: place.address,
+            ),
+          ),
+        );
+      }
+    }
+
+    // Add markers for the dummy places
+    _addDummyMarkers();
+
+    setState(() {}); // Rebuild the widget after updating markers
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: _center == null
+                    ? const CameraPosition(
+                        target: LatLng(37.4910133, 126.7206483),
+                        zoom: 11.0,
+                      )
+                    : CameraPosition(
+                        target: _center,
+                        zoom: 15.0,
+                      ),
+                markers: _markers,
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _getCurrentLocation,
+          child: Icon(Icons.my_location),
+          backgroundColor: Colors.blue[700],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      ),
+    );
+  }
 }
-
-
--- lib/map/maptotal.dart
+++ lib/map/maptotal.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geocoding/geocoding.dart';
 
-void main() => runApp(MyApp());
+Future<LatLng?> getLocationFromAddress(String address) async {
+  try {
+    List<Location> locations = await locationFromAddress(address);
+    if (locations.isNotEmpty) {
+      Location location = locations.first;
+      return LatLng(location.latitude, location.longitude);
+    }
+    return null;
+  } catch (e) {
+    print('Error: $e');
+    return null;
+  }
+}
+
+class Place {
+  final String name;
+  final String address;
+  final Future<LatLng?> location; // 위치를 비동기로 받음
+
+  Place({required this.name, required this.address})
+      : location = getLocationFromAddress(address);
+}
+
+List<Place> places = [
+  Place(name: '칸다소바', address: '인천 부평구 부평대로36번길 5'),
+  Place(name: '인브스키친', address: '인천 부평구 부평대로 39-6'),
+  Place(name: '에픽', address: '인천 부평구 경원대로1363번길 8'),
+  // 다른 더미 데이터들도 동일한 방식으로 추가할 수 있음
+];
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -13,9 +45,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
   late LatLng _center = LatLng(37.4895, 126.7220);
-  TextEditingController _addressController = TextEditingController();
-  String _latitude = '';
-  String _longitude = '';
 
   Set<Marker> _markers = {};
 
@@ -23,48 +52,61 @@ class _MyAppState extends State<MyApp> {
     mapController = controller;
   }
 
-  Future<void> _getGeolocationData() async {
-    String address = _addressController.text;
+  @override
+  void initState() {
+    super.initState();
+    _addMarkers();
+  }
 
-    try {
-      List<Location> locations = await locationFromAddress(address);
-      if (locations.isNotEmpty) {
-        Location location = locations.first;
-        setState(() {
-          _latitude = location.latitude.toString();
-          _longitude = location.longitude.toString();
-
-          // Clear previous markers
-          _markers.clear();
-
-          // Add marker for the searched address
-          _markers.add(
-            Marker(
-              markerId: MarkerId('searched_location'),
-              position: LatLng(location.latitude, location.longitude),
-              infoWindow: InfoWindow(
-                title: 'Searched Location',
-                snippet: 'Lat: ${location.latitude}, Lng: ${location.longitude}',
-              ),
+  Future<void> _addMarkers() async {
+    for (var place in places) {
+      LatLng? location = await place.location;
+      if (location != null) {
+        _markers.add(
+          Marker(
+            markerId: MarkerId(place.name),
+            position: location,
+            infoWindow: InfoWindow(
+              title: place.name,
+              snippet: place.address,
             ),
-          );
-
-          // Move camera to the searched location
-          mapController.animateCamera(
-            CameraUpdate.newCameraPosition(
-              CameraPosition(
-                target: LatLng(location.latitude, location.longitude),
-                zoom: 15.0,
-              ),
-            ),
-          );
-        });
-      } else {
-        setState(() {
-          _latitude = 'N/A';
-          _longitude = 'N/A';
-        });
+          ),
+        );
       }
+    }
+    setState(() {}); // 마커를 추가한 후 화면을 갱신하여 지도에 마커를 표시
+  }
+
+  Future<void> _getCurrentMyLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      LatLng myLocation = LatLng(position.latitude, position.longitude);
+
+      mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: myLocation,
+            zoom: 18.0,
+          ),
+        ),
+      );
+
+      // 추가: 사용자의 위치를 마커로 표시
+      _markers.add(
+        Marker(
+          markerId: MarkerId('my_location'),
+          position: myLocation,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue), // 파란색 마커 아이콘 사용
+          infoWindow: InfoWindow(
+            title: 'My Location',
+            snippet: 'Lat: ${position.latitude}, Lng: ${position.longitude}',
+          ),
+        ),
+      );
+      setState(() {}); // 위젯을 리빌드하여 마커를 지도에 표시
     } catch (e) {
       print('Error: $e');
     }
@@ -80,43 +122,29 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  TextField(
-                    controller: _addressController,
-                    decoration: InputDecoration(labelText: 'Enter Address'),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: _getGeolocationData,
-                    child: Text('Get Geolocation'),
-                  ),
-                  SizedBox(height: 20),
-                  Text('Latitude: $_latitude'),
-                  Text('Longitude: $_longitude'),
-                ],
-              ),
-            ),
             Expanded(
               child: GoogleMap(
                 onMapCreated: _onMapCreated,
                 initialCameraPosition: _center == null
                     ? const CameraPosition(
-                  target: LatLng(37.4910133, 126.7206483),
-                  zoom: 11.0,
-                )
+                        target: LatLng(37.4910133, 126.7206483),
+                        zoom: 11.0,
+                      )
                     : CameraPosition(
-                  target: _center,
-                  zoom: 15.0,
-                ),
+                        target: _center,
+                        zoom: 15.0,
+                      ),
                 markers: _markers,
               ),
             ),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _getCurrentMyLocation,
+          child: Icon(Icons.my_location),
+          backgroundColor: Colors.blue[700],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       ),
     );
   }
