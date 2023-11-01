75e37b85746b2b18984b0448b7c92c4bc4e438f5
olsang35@naver.com <olsang310@gmail.com>
2023-11-01 11:07:05 +0900

    fm 1101_sy main

--- lib/main/mainPage.dart
+++ lib/main/mainPage.dart
@@ -24,6 +24,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final PageController _pageController = PageController(initialPage: 0);
+  PageController _horizontalPageController = PageController(initialPage: 0);
   int _currentPage = 0;
 
   final List<String> images = [
@@ -32,6 +33,18 @@ class _MainPageState extends State<MainPage> {
     'assets/main/steak.jpg',
     'assets/main/whine.jpg',
     'assets/main/whisky.png',
+  ];
+
+  final List<String> horizontalImages = [
+    'assets/main/fine.jpg',
+    'assets/main/gong.jpg',
+    'assets/main/steak.jpg',
+    // 여기에 추가 이미지 경로를 추가할 수 있습니다.
+  ];
+  final List<Widget> horizontalPages = [
+    Sample13(),
+    Sample13(),
+    Sample13(),
   ];
 
   final List<ButtonData> buttonsData = [
@@ -52,9 +65,12 @@ class _MainPageState extends State<MainPage> {
     ButtonData('assets/main/pojang.png', '포장마차'),
   ];
 
+  Timer? _timer;
+
   void initState() {
     super.initState();
-    Timer.periodic(Duration(seconds: 6), (Timer timer) {
+    _timer?.cancel();
+    _timer  = Timer.periodic(Duration(seconds: 5), (Timer timer) {
       if (_currentPage < images.length - 1) {
         _currentPage++;
       } else {
@@ -66,6 +82,7 @@ class _MainPageState extends State<MainPage> {
         curve: Curves.easeIn,
       );
     });
+    _horizontalPageController = PageController(initialPage: 0);
   }
 
   Widget buildIconWithText(String imagePath, String text, VoidCallback onTap) {
@@ -123,9 +140,31 @@ class _MainPageState extends State<MainPage> {
     ];
 
     if (index >= 0 && index < pages.length) {
+      int totalPages = pages.length;
       Navigator.push(context, MaterialPageRoute(builder: (context) => pages[index]));
+
+      // 페이지가 자동으로 슬라이드되도록 타이머 설정
+      Timer? autoSlideTimer;
+      autoSlideTimer = Timer.periodic(Duration(seconds: 6), (timer) {
+        if (_currentPage < totalPages - 1) {
+          _currentPage++;
+        } else {
+          _currentPage = 0;
+        }
+        _pageController.animateToPage(
+          _currentPage,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeIn,
+        );
+
+        // 타이머 취소
+        if (_currentPage == 0) {
+          autoSlideTimer?.cancel();
+        }
+      });
     }
-  } void navigatePlacePage(int index) {
+  }
+  void navigatePlacePage(int index) {
     final pages = [
       Sample13(),
       Sample13(),
@@ -196,97 +235,103 @@ class _MainPageState extends State<MainPage> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
         children: [
-          SizedBox(height: 1),
-          Expanded(
-            child: PageView.builder(
-              controller: _pageController,
-              itemCount: images.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    navigateEventPage(index);
-                  },
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    child: Image.asset(
-                      images[index],
-                      height: 300.0,
-                      width: 500.0,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 5,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
-              ),
-              itemCount: buttonsData.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    navigateToPage(index);
-                  },
-                  child: Column(
-                    children: [
-                      ClipOval(
+          Column(
+            children: [
+              SizedBox(height: 15,),
+              Container(
+                height: 250,
+                width: double.infinity,
+                child: PageView.builder(
+                    itemCount: images.length,
+                    controller: _pageController,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: (){
+                          navigateEventPage(index);
+                        },
                         child: Image.asset(
-                          buttonsData[index].image,
-                          fit: BoxFit.cover,
-                          width: 52,
-                          height: 52,
-                        ),
+                            images[index],
+                        height: 200.0,
+                        fit: BoxFit.cover,),
+                      );
+                    }),
+              ),
+              SizedBox(height: 20,),
+              GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 5,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                  ),
+                  itemCount: buttonsData.length,
+                  itemBuilder: (context, index){
+                    return InkWell(
+                      onTap: (){
+                        navigateToPage(index);
+                      },
+                      child: Column(
+                        children: [
+                          ClipOval(
+                            child: Image.asset(
+                                buttonsData[index].image,
+                            fit: BoxFit.cover,
+                            width: 52,
+                            height: 52,),
+                          ),
+                          Text(buttonsData[index].text,
+                          style: TextStyle(fontSize: 16),)
+                        ],
                       ),
-                      Text(
-                        buttonsData[index].text,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                        ),
-                      ),
+                    );
+                  }),
+              SizedBox(height: 20,),
+              Container(height: 5, width: 400, color: Colors.grey[300]!),
+              SizedBox(height: 20,),
+              Row(
+
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(width: 15,),
+                  Text("어디로 가시나요?",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
+                ],
+              ),
+              SizedBox(height: 10,),
+              Container(
+                height: 200,
+                width: double.infinity,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      buildIconWithText('assets/main/gps-removebg-preview.png', '내주변', () {
+                        navigatePlacePage(5);
+                      }),
+                      buildIconWithText('assets/main/incheon.jpg', '인천/부평', () {
+                        navigatePlacePage(0);
+                      }),
+                      buildIconWithText('assets/main/incheon2.jpg', '인천/연수', () {
+                        navigatePlacePage(1);
+                      }),
+                      buildIconWithText('assets/main/seoul.jpg', '서울/동암', () {
+                        navigatePlacePage(2);
+                      }),
+                      buildIconWithText('assets/main/seoul2.jpg', '서울/중구', () {
+                        navigatePlacePage(3);
+                      }),
+                      buildIconWithText('assets/main/seoul3.jpg', '서울/남동구', () {
+                        navigatePlacePage(4);
+                      }),
                     ],
                   ),
-                );
-              },
-            ),
+                ),
+              )
+
+            ],
           ),
-          SizedBox(height: 15),
-          Text(
-            "  어디로 갈래?",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFFFF6347)),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                buildIconWithText('assets/main/gps-removebg-preview.png', '내주변', () {
-                  navigatePlacePage(5);
-                }),
-                buildIconWithText('assets/main/incheon.jpg', '인천/부평', () {
-                  navigatePlacePage(0);
-                }),
-                buildIconWithText('assets/main/incheon2.jpg', '인천/연수', () {
-                  navigatePlacePage(1);
-                }),
-                buildIconWithText('assets/main/seoul.jpg', '서울/동암', () {
-                  navigatePlacePage(2);
-                }),
-                buildIconWithText('assets/main/seoul2.jpg', '서울/중구', () {
-                  navigatePlacePage(3);
-                }),
-                buildIconWithText('assets/main/seoul3.jpg', '서울/남동구', () {
-                  navigatePlacePage(4);
-                }),
-              ],
-            ),
-          ),SizedBox(height: 10),
         ],
       ),
       bottomNavigationBar: BottomNavBar(),
