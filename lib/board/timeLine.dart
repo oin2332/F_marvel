3aa4eecbbc170d2cea205b7b0c5bd5c7dca10758
bogyuuggi <sss0832@naver.com>
2023-11-03 20:30:01 +0900

    231103_board_bg

--- lib/board/timeLine.dart
+++ lib/board/timeLine.dart
@@ -229,6 +229,61 @@ class _TimeLineState extends State<TimeLine> {
                       ],
                     ),
                   ),
+                  Container(height: 10, width: 410, color: Colors.grey[300]!),
+                  Row(
+                    children: [
+                      ClipOval(
+                        child: Image.asset(
+                          'assets/user/basic.jpg',
+                          width: 45,
+                          height: 45,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('식도성역류염', style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text('리뷰10개, 평균 별점 5.0') // 수정 필요한 부분
+                        ],
+                      ),
+                      SizedBox(width: 115),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.deepOrange[400], // 배경 색상
+                          ),
+                          onPressed: (){},
+                          child: Text('팔로우')
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Image.asset('assets/user/review4.jpg'),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.star),
+                      Text('18일전')
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('홀리쉿')
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(onPressed: (){}, icon: Icon(Icons.favorite)),
+                        SizedBox(width: 30),
+                        IconButton(onPressed: (){}, icon: Icon(Icons.messenger))
+                      ],
+                    ),
+                  ),
                   Container(height: 10, width: 410, color: Colors.grey[300]!)
                 ],
               ),
@@ -247,12 +302,12 @@ class _TimeLineState extends State<TimeLine> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20), // 타원형 모양을 위한 값
         ),
-        primary: Colors.white, // 배경색을 흰색으로 설정
+        primary: Colors.deepOrange[400]!, // 배경색을 흰색으로 설정
       ),
       child: Text(
         text,
         style: TextStyle(
-          color: Colors.black, // 글씨를 검은색으로 설정
+          color: Colors.white, // 글씨를 검은색으로 설정
         ),
       ),
     );
