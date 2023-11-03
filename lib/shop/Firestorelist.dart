3144596209ea9705ce90310a20c40ca46e3a46a4
tj-bu-706-15 <tx214@naver.com>
2023-11-03 15:44:31 +0900

    Commit_dh

--- lib/shop/Firestorelist.dart
+++ lib/shop/Firestorelist.dart
@@ -1,6 +1,33 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../firebase/firebase_options.dart';
+import '../user/userModel.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => UserModel(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: teee(),
+    );
+  }
+}
 
 class teee extends StatefulWidget {
   const teee({super.key});
@@ -14,28 +41,28 @@ class _teeeState extends State<teee> {
   @override
   void initState() {
     super.initState();
-    fetchAllUserData1();
-    fetchAllUserData2();
+    fetchAllUserData();
   }
 
 
   List<Map<String, dynamic>> userDataList = [];
 
-  void fetchAllUserData1() async {
+  void fetchAllUserData() async {
     try {
       QuerySnapshot storeSnapshot = await FirebaseFirestore.instance
           .collection('T3_STORE_TBL')
           .get();
 
+
       if (storeSnapshot.docs.isNotEmpty) {
         for (var storeDoc in storeSnapshot.docs) {
           Map<String, dynamic> storeData = storeDoc.data() as Map<String, dynamic>;
-          String sId = storeData['S_ID'];
+          String docId = storeDoc.id;
 
           // 해당 상점의 별점 정보 가져오기
           QuerySnapshot starSnapshot = await FirebaseFirestore.instance
               .collection('T3_STORE_TBL')
-              .doc(sId)
+              .doc(docId)
               .collection('T3_STAR_TBL')
               .get();
 
@@ -43,24 +70,37 @@ class _teeeState extends State<teee> {
           if (starSnapshot.docs.isNotEmpty) {
             for (var starDoc in starSnapshot.docs) {
               Map<String, dynamic> starData = starDoc.data() as Map<String, dynamic>;
-              starDataList.add({'STAR': starData['STAR']});
+              starDataList.add(starData);
+
             }
+          } else {
+            // 별점 정보가 없는 경우, 빈 리스트로 처리하거나 필요에 따라 다른 처리를 수행
+            starDataList = [];
+
           }
 
-          // userDataList에 상점 정보와 별점 정보를 조합하여 추가
-          userDataList.add({'S_ID': sId, 'STAR_LIST': starDataList, ...storeData});
-        }
+          // storeData에 'STAR' 키를 사용하여 starDataList 추가
+          storeData['STAR'] = starDataList;
 
+          userDataList.add(storeData);
+
+
+        }
         setState(() {
+
           // 데이터가 추가된 userDataList를 화면에 반영
         });
       } else {
         print('상점 데이터를 찾을 수 없습니다.');
+
       }
     } catch (e) {
       print('데이터를 불러오는 중 오류가 발생했습니다: $e');
+
     }
   }
+
+
 
 
   @override
