b2840a2f4be55031d540f8e7670d84d3c70cabea
bogyuuggi <sss0832@naver.com>
2023-11-08 11:45:50 +0900

    231108_yaml_bg

--- lib/board/boardAdd.dart
+++ lib/board/boardAdd.dart
@@ -58,85 +58,36 @@ class _BoardAddState extends State<BoardAdd> {
     }
   }
 
-  Widget _listUser() {
-    return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection("board").orderBy("timestamp", descending: true).snapshots(),
-      ///////////////////users/////////////////////
-      //게시글 정렬 후 출력 (orderBy(descending: ,))
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snap){
-        if(!snap.hasData) {
-          return Transform.scale(
-            scale: 0.1,
-            child: CircularProgressIndicator(strokeWidth: 20),
-          );
-        }
-        return ListView(
-          children: snap.data!.docs.map(
-                (DocumentSnapshot document) {
-              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-              return RadioListTile(
-                title: Text(data['title']),
-                subtitle: Text("내용 : ${data['content']}"),
-                value: document.id, // 선택된 값을 Document ID로 설정
-                groupValue: _selectUser,
-                onChanged: (value) {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => BoardView(document: document),),);
-                  setState(() {
-                    _selectUser = value;
-                    _title.text = data['title'];
-                    _content.text = data['content'].toString();
-                  });
-                },
-              );
-            },
-          ).toList(),
-        );
-      },
-    );
-  }
-
-  void _selectPic() {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.photo),
-              title: Text('앨범에서 선택'),
-              onTap: () {
-                // 앨범에서 선택하는 로직을 추가하세요
-                // 예를 들어, 이미지 선택 코드를 넣어주세요.
-                Navigator.pop(context); // 모달 바텀 시트 닫기
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.camera),
-              title: Text('카메라로 촬영'),
-              onTap: () {
-                // 카메라로 촬영하는 로직을 추가하세요
-                Navigator.pop(context); // 모달 바텀 시트 닫기
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
+  // 이미지 선택 + 업로드 + 미리보기
   List<File> _selectedImages = []; // 선택된 이미지들을 저장하는 리스트
+
+  Image? _selectedImage; // 선택된 이미지를 저장하는 변수
+
+  // void _selectPic() async {
+  //   final imagePicker = ImagePicker();
+  //
+  //   // 앨범에서 사진을 선택
+  //   final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery); // 단일 이미지 선택
+  //
+  //   if (pickedFile != null) {
+  //     setState(() {
+  //       _selectedImage = Image.file(File(pickedFile.path)); // 선택된 파일을 File로 변환하여 리스트에 추가
+  //     });
+  //   }
+  // }
+
   void _selectPics() async {
     final imagePicker = ImagePicker();
 
-    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery); // 단일 이미지 선택
+    final pickedFiles = await imagePicker.pickMultiImage(); // 다중 이미지 선택
 
-    if (pickedFile != null) {
+    if (pickedFiles != null) {
       setState(() {
-        _selectedImages.add(File(pickedFile.path)); // 선택된 파일을 File로 변환하여 리스트에 추가
+        _selectedImages.addAll(pickedFiles.map((pickedFile) => File(pickedFile.path)).toList());
       });
     }
   }
+
 
   // 별점 기능
   double _rating = 0.0; // 초기 별점 설정
@@ -157,21 +108,7 @@ class _BoardAddState extends State<BoardAdd> {
     uId = userId;
     print('현재 로그인 아이디 : $uId');
 
-    Image? _selectedImage; // 선택된 이미지를 저장하는 변수
-    List<File> _selectedImages = []; // 선택된 이미지들을 저장하는 리스트
 
-    void _selectPic() async {
-      final imagePicker = ImagePicker();
-
-      // 앨범에서 사진을 선택
-      final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
-
-      if (pickedFile != null) {
-        setState(() {
-          _selectedImage = Image.file(File(pickedFile.path));
-        });
-      }
-    }
 
     return Scaffold(
         appBar: AppBar(
@@ -192,7 +129,7 @@ class _BoardAddState extends State<BoardAdd> {
               shrinkWrap: true,
               children: [
                 Container(
-                  width: 80,
+                  width: double.infinity,
                   height: 250,
                   decoration: BoxDecoration(
                     border: Border.all(color: Colors.black),
@@ -201,12 +138,24 @@ class _BoardAddState extends State<BoardAdd> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       IconButton(
-                        onPressed: _selectPic,
+                        onPressed: _selectPics,
                         icon: Icon(Icons.add_circle),
                       ),
-                      if (_selectedImage != null) _selectedImage!,
-                      if (_selectedImage == null) Text('사진 추가'),
-                      Text('0 / 15')
+                      SizedBox(height: 10),
+                      if (_selectedImages.isNotEmpty)
+                        GridView.count(
+                          crossAxisCount: 2,
+                          shrinkWrap: true,
+                          children: List.generate(_selectedImages.length, (index) {
+                            return Container(
+                              height: 100,
+                              child: Image.file(_selectedImages[index]),
+                            );
+                          }),
+                        ),
+                      if (_selectedImages.isEmpty)
+                        Text('사진 추가'),
+                      Text('${_selectedImages.length} / 15')
                     ],
                   ),
                 ),
@@ -257,9 +206,9 @@ class _BoardAddState extends State<BoardAdd> {
                     });
                   },
                   ratingWidget: RatingWidget(
-                    full: Icon(Icons.star),
-                    half: Icon(Icons.star_half),
-                    empty: Icon(Icons.star_border),
+                    full: Icon(Icons.star, color: Colors.amber),
+                    half: Icon(Icons.star_half, color: Colors.amber),
+                    empty: Icon(Icons.star_border, color: Colors.amber),
                   ),
                 ),
                 ElevatedButton(
