37fc738b6b35717f23a3c280975d31354fd9af23
saaita <saaita@naver.com>
2023-11-09 21:09:50 +0900

    이이잉2

--- lib/reservation/function/bookmark.dart
+++ lib/reservation/function/bookmark.dart
@@ -1,25 +1,43 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<void> addBookmark(String userId, String docId) async {
+void addBookmark(String userId, String docId) async {
   try {
     FirebaseFirestore fs = FirebaseFirestore.instance;
     CollectionReference stores = fs.collection("T3_STORE_TBL");
-    DocumentReference bookmarkRef = stores.doc(docId).collection("T3_BOOKMARK_TBL").doc(userId);
 
-    DocumentSnapshot bookmarkDoc = await bookmarkRef.get();
+    await stores
+        .doc(docId)
+        .collection("T3_BOOKMARK_TBL")
+        .doc(userId)
+        .set({
+      'timestamp': FieldValue.serverTimestamp(),
+      'Booktime': "Y",
+    });
 
-    if (bookmarkDoc.exists) {
-      // 북마크가 이미 존재하면 제거
-      await bookmarkRef.delete();
-      print('북마크를 제거했습니다. 사용자 아이디 >> $userId');
-    } else {
-      // 북마크가 존재하지 않으면 추가
-      await bookmarkRef.set({
-        'timestamp': FieldValue.serverTimestamp(),
-      });
-      print('북마크를 추가했습니다. 사용자 아이디 >> $userId');
-    }
+    print('북마크에 추가 할 사용자 아이디 >> $userId');
+
+    // 10초 후에 Booktime 값을 "N"으로 업데이트
+    Future.delayed(Duration(seconds: 10), () async {
+      await updateBooktimeToN(docId, userId);
+    });
   } catch (e) {
     print('북마크 작업 중 오류 발생: $e');
+  }
+}
+
+Future<void> updateBooktimeToN(String docId, String userId) async {
+  try {
+    FirebaseFirestore fs = FirebaseFirestore.instance;
+    CollectionReference stores = fs.collection("T3_STORE_TBL");
+
+    await stores
+        .doc(docId)
+        .collection("T3_BOOKMARK_TBL")
+        .doc(userId)
+        .update({'Booktime': 'N'});
+
+    print('Booktime을 "N"으로 업데이트: $userId');
+  } catch (e) {
+    print('Booktime 업데이트 중 오류 발생: $e');
   }
 }
--- lib/search/ImportSuddenpopular.dart
+++ lib/search/ImportSuddenpopular.dart
@@ -1,102 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ImportSuddenPopular extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Text(
-            "관심 급상승 음식점",
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        Row(
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('T3_STORE_TBL').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return CircularProgressIndicator();
+        }
+        var stores = snapshot.data!.docs.toList();
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Center(
-                child: Wrap(
-                  alignment: WrapAlignment.start,
-                  children: [
-                    for (int i = 1; i <= 5; i++)
-                      Container(
-                        margin: EdgeInsets.all(15.0),
-                        padding: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Row(
-                          children: [
-                            Text(
-                              "$i",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                height: 1.5,
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(left: 15.0),
-                              child: Text(
-                                "음식점",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                  ],
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                "관심 급상승 음식점",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            Expanded(
-              child: Center(
-                child: Wrap(
-                  alignment: WrapAlignment.start,
-                  children: [
-                    for (int i = 6; i <= 10; i++)
-                      Container(
-                        margin: EdgeInsets.all(15.0),
-                        padding: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Row(
-                          children: [
-                            Text(
-                              "$i",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                height: 1.5,
-                              ),
-                            ),
+            FutureBuilder(
+              future: Future.wait(stores.map((store) async {
+                var bookmarkSnapshot = await FirebaseFirestore.instance
+                    .collection('T3_STORE_TBL')
+                    .doc(store.id)
+                    .collection('T3_BOOKMARK_TBL')
+                    .where('Booktime', isEqualTo: 'Y')
+                    .get();
+                int bookmarkCount = bookmarkSnapshot.docs.length;
+
+
+                return {'store': store, 'bookmarkCount': bookmarkCount};
+              })),
+              builder: (context, storeSnapshots) {
+                if (storeSnapshots.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+
+                if (storeSnapshots.hasError) {
+                  return Text("Error: ${storeSnapshots.error}");
+                }
+
+                var sortedStores = (storeSnapshots.data as List)
+                  ..sort((a, b) {
+                    int aCount = a['bookmarkCount'] as int;
+                    int bCount = b['bookmarkCount'] as int;
+                    return bCount.compareTo(aCount);
+                  });
+
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          for (var i = 0; i < 5; i++)
                             Container(
-                              margin: EdgeInsets.only(left: 15.0),
+                              margin: EdgeInsets.only(top:15.0,right: 15.0,bottom: 15.0,left: 10),
+                              padding: EdgeInsets.only(top:10.0,right: 10.0,bottom: 10.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+
+                              ),
                               child: Text(
-                                "음식점",
+                                "${i + 1}. ${sortedStores[i]['store']['S_NAME']} (${sortedStores[i]['bookmarkCount']})",
                                 style: TextStyle(
                                   fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  height: 1.5,
                                 ),
                               ),
                             ),
-                          ],
-                        ),
+                        ],
                       ),
-                  ],
-                ),
-              ),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          for (var i = 5; i < 10; i++)
+                            Container(
+                              margin: EdgeInsets.all(15.0),
+                              padding: EdgeInsets.only(top:10.0,right: 10.0,bottom: 10.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Text(
+                                "${i + 1}. ${sortedStores[i]['store']['S_NAME']} (${sortedStores[i]['bookmarkCount']})",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  height: 1.5,
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ],
-        ),
-      ],
+        );
+      },
     );
   }
 }
+
