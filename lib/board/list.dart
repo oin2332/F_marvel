bab1884e71749412d2e01588f1d9ce45c8a9b88f
bogyuuggi <sss0832@naver.com>
2023-11-10 20:26:29 +0900

    231110_board_bg

--- lib/board/list.dart
+++ lib/board/list.dart
@@ -1,5 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:food_marvel/board/function/Board.dart';
+
 
 class Temp extends StatefulWidget {
   const Temp({super.key});
@@ -11,75 +12,102 @@ class Temp extends StatefulWidget {
 class _TempState extends State<Temp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('임시리스트')),
-      body: ListView(
-        shrinkWrap: true,
-        children: [
-          _listReview()
-        ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text('타임라인', style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(60.0),
+            child: Column(
+              children: [
+                TabBar(
+                  tabs: [
+                    Tab(text: '추천'),
+                    Tab(text: '팔로잉'),
+                  ],
+                  labelColor: Colors.black,
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  unselectedLabelColor: Colors.grey,
+                  indicatorColor: Colors.red,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 5),
+                      _menubutton('오마카세'),
+                      SizedBox(width: 5),
+                      _menubutton('스테이크'),
+                      SizedBox(width: 5),
+                      _menubutton('카페'),
+                      SizedBox(width: 5),
+                      _menubutton('한식'),
+                      SizedBox(width: 5),
+                      _menubutton('중식'),
+                      SizedBox(width: 5),
+                      _menubutton('일식'),
+                      SizedBox(width: 5),
+                      _menubutton('양식'),
+                      SizedBox(width: 5),
+                      _menubutton('아시안'),
+                      SizedBox(width: 5),
+                      _menubutton('치킨'),
+                      SizedBox(width: 5),
+                      _menubutton('피자'),
+                      SizedBox(width: 5),
+                      _menubutton('햄버거'),
+                      SizedBox(width: 5),
+                      _menubutton('분식'),
+                      SizedBox(width: 5),
+                      _menubutton('포장마차'),
+                      SizedBox(width: 5),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            RecomendTab(),
+            FollowingTab(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _menubutton(String text) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20), // 타원형 모양을 위한 값
+          side: BorderSide(color: Colors.grey[300]!), // 테두리 색상을 변경
+        ),
+        primary: Colors.white, // 배경색을 흰색으로 설정
+        onPrimary: Colors.deepOrangeAccent[100]!, // 선택된 상태일 때 버튼 색상 변경
+        onSurface: Colors.deepOrangeAccent[400]!, // 선택된 상태일 때 텍스트 색상 변경
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.grey[600]!, // 글씨를 검은색으로 설정
+        ),
       ),
     );
   }
 }
 
 
-Widget _listReview() {
-  return StreamBuilder<QuerySnapshot>(
-    stream: FirebaseFirestore.instance.collection('T3_REVIEW_TBL').snapshots(),
-    builder: (context, snapshot) {
-      if (!snapshot.hasData) {
-        return CircularProgressIndicator(); // 데이터를 불러올 때까지 로딩 중을 표시합니다.
-      }
-      var reviews = snapshot.data!.docs; // 데이터가 있는 경우 문서 리스트를 가져옵니다.
-      List<Widget> reviewWidgets = [];
-      for (var review in reviews) {
-        var title = review['title'];
-        var content = review['content'];
-        var imageUrls = review['r_img_urls']; // 이미지 URL 리스트
-        var rating = review['rating'];
 
-        var imageSlider = Container(
-          height: 400, // 슬라이드 높이 설정
-          child: ListView(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            children: List.generate(imageUrls.length, (index) {
-              return Container(
-                width: 400, // 이미지의 폭을 동일하게 설정
-                child: Image.network(
-                  imageUrls[index],
-                  fit: BoxFit.cover, // 이미지를 화면에 맞게 조절
-                ),
-              );
-            }),
-          ),
-        );
-
-        var reviewWidget = ListTile(
-          title: Text(title),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(content),
-              Row(
-                children: [
-                  Icon(Icons.star, color: Colors.amber),
-                  Text(rating.toString()),
-                ],
-              ),
-              imageSlider, // 슬라이더 추가
-            ],
-          ),
-        );
-
-        reviewWidgets.add(reviewWidget);
-      }
-
-      return ListView(
-        shrinkWrap: true,
-        children: reviewWidgets,
-      );
-    },
-  );
-}
--- lib/user/function/Follow.dart
+++ lib/user/function/Follow.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-// 팔로우 (팔로워) 기능
+// 팔로우 (팔로워) 조회
 Future<List<String>> getUserFollowers(String userId) async {
   List<String> followers = [];
 
@@ -15,8 +15,9 @@ Future<List<String>> getUserFollowers(String userId) async {
       // 사용자 문서가 존재하는 경우에만 실행
 
       followerDocSnapshot.docs.forEach((doc) {
-        followers.add(doc.id);
-        print('추가된 팔로워 ID: ${doc.id}');
+        followers.add(doc.id); // 내가 추가한 사람의 팔로워 리스트에 내 아이디 추가
+        //내 팔로잉 리스트에 내가 팔로우 한사람 아이디 추가
+        print('내가 추가한 팔로워 ID: ${doc.id}');
       });
     }
 
@@ -27,7 +28,7 @@ Future<List<String>> getUserFollowers(String userId) async {
   }
 }
 
-// 팔로잉 기능
+// 팔로잉 조회
 Future<List<String>> getUserFollowings(String userId) async {
   List<String> followings = [];
 
@@ -53,3 +54,45 @@ Future<List<String>> getUserFollowings(String userId) async {
     return [];
   }
 }
+
+//팔로우 기능
+Future<void> followUser(String myUserId, String followedUserId) async {
+  // String myUserId <- 현재 로그인 아이디
+  // String followedUserId <- 현재 아이디가 팔로우 하려는 아이디
+  try {
+    CollectionReference userCollection = FirebaseFirestore.instance.collection(
+        'T3_USER_TBL');
+    var userDocSnapshot = await userCollection.where('id', isEqualTo: myUserId)
+        .limit(1)
+        .get();
+    var followedUserDocSnapshot = await userCollection.where(
+        'id', isEqualTo: followedUserId).limit(1).get();
+
+    // 1. 내 아이디로 로그인하여 다른 사용자를 팔로우하는 경우
+    // t3_follower_tbl 컬렉션에 새로운 문서를 생성하고 timestamp 필드를 추가
+
+    if (userDocSnapshot.docs.isNotEmpty) {
+      await followedUserDocSnapshot.docs.first.reference.collection(
+          'T3_FOLLOWER_TBL').doc(myUserId).set({
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+      print('현재 사용자 팔로잉 증가');
+    }
+
+    // 2. 동시에 내 유저 정보를 담은 문서에서 t3_following_tbl 서브컬렉션에 새로운 문서를 생성
+    // followedUserId를 문서 아이디로 사용하여 새로운 문서를 생성
+
+    if (followedUserDocSnapshot.docs.isNotEmpty) {
+      await userDocSnapshot.docs.first.reference.collection('T3_FOLLOWING_TBL')
+          .doc(followedUserId)
+          .set({
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+      print('다른 사용자 팔로워 증가');
+    }
+  } catch (e) {
+    print('팔로우 에러: $e');
+  }
+}
+
+
