89e981d325883942bf5c3ceb36f5851ee1bf371d
Son <smo3072@naver.com>
2023-11-16 10:46:58 +0900

    /1116/son

--- lib/main/ImportWhere.dart
+++ lib/main/ImportWhere.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:food_marvel/map/localmap.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../shop/Addshop/storeboardAdd.dart';
 import '../map/maptotal.dart';
+import '../shop/Addshop/storemenuimgAdd2.dart';
 
 
 class ImportWhere extends StatefulWidget {
@@ -29,6 +34,20 @@ class _ImportWhereState extends State<ImportWhere> {
       ],
     );
   }
+  void navigatePlacePage(int index) {
+    final pages = [
+      GooGleMap(),
+      SMap(initialLocation: LatLng(37.4563, 126.7052)),
+      SMap(initialLocation: LatLng(37.5665, 126.9780)),
+      SMap(initialLocation: LatLng(37.4138, 127.5185)),
+      SMap(initialLocation: LatLng(33.4996, 126.5312)),
+      //Sample13(),
+    ];
+
+    if (index >= 0 && index < pages.length) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => pages[index]));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,19 +73,19 @@ class _ImportWhereState extends State<ImportWhere> {
                   Navigator.push(context, MaterialPageRoute(builder: (context) => GooGleMap()));
                 }),
                 buildIconWithText('assets/main/incheon.jpg', '인천', () {
-
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SMap(initialLocation: LatLng(37.4563, 126.7052))));
                 }),
                 buildIconWithText('assets/main/seoul2.jpg', '서울', () {
-
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SMap(initialLocation: LatLng(37.5665, 126.9780))));
                 }),
                 buildIconWithText('assets/main/rudrleh.jpg', '경기', () {
-
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SMap(initialLocation: LatLng(37.4138, 127.5185))));
                 }),
                 buildIconWithText('assets/main/wpwneh.jpg', '제주', () {
-
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SMap(initialLocation: LatLng(33.4996, 126.5312))));
                 }),
                 // buildIconWithText('assets/main/seoul3.jpg', '서울/남동구', () {
-
+                //   navigatePlacePage(4);
                 // }),
               ],
             ),
---
+++ lib/map/localmap.dart
@@ -0,0 +1,374 @@
+import 'package:flutter/material.dart';
+import 'package:food_marvel/map/function/circleboundary.dart';
+import 'package:food_marvel/map/function/geocoding.dart';
+import 'package:food_marvel/map/function/getdbdata.dart';
+import 'package:food_marvel/map/maptotal.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import '../firebase/firebase_options.dart';
+
+class Place {
+  final String name;
+  final String address;
+  final Future<LatLng?> location;
+
+  Place({required this.name, required this.address, required String category})
+      : location = getLocationFromAddress(address);
+}
+
+class SMap extends StatefulWidget {
+  final LatLng initialLocation;
+  const SMap({Key? key, required this.initialLocation}) : super(key: key);
+
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<SMap> {
+  late GoogleMapController mapController;
+  LatLng _center = LatLng(37.5665, 126.9780); // 서울의 좌표로 초기화
+  double _zoomLevel = 13.0; // 초기 줌 레벨을 33배로 설정
+  LatLng? _selectedLocation;
+
+  Set<Marker> _markers = {};
+  Set<Circle> _circles = {};
+
+  String _searchQuery = "";
+  String _selectedCategory = '';
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _center = widget.initialLocation; // 초기 위치 설정
+    _addMarkers();
+  }
+
+  Future<void> _addMarkers() async {
+    List<Place> places = (await getData()).cast<Place>();
+
+    _circles.add(getCircleBoundary(_center, 600));
+
+    if (_circles.isEmpty) {
+      print('Circle is not available.');
+      return;
+    }
+
+    LatLng circleCenter = _circles.first.center;
+    double circleRadius = _circles.first.radius;
+
+    for (var place in places) {
+      LatLng? location = await place.location;
+      if (location != null) {
+        double distance = Geolocator.distanceBetween(
+          circleCenter.latitude,
+          circleCenter.longitude,
+          location.latitude,
+          location.longitude,
+        );
+
+        if (distance <= circleRadius) {
+          _markers.add(
+            Marker(
+              markerId: MarkerId(place.name),
+              position: location,
+              infoWindow: InfoWindow(
+                title: place.name,
+                snippet: place.address,
+              ),
+            ),
+          );
+        }
+      }
+    }
+
+    setState(() {});
+  }
+
+  void _filterPlaces(String query) {
+    setState(() {
+      _searchQuery = query;
+      _addMarkers();
+    });
+  }
+
+  Future<void> _getCurrentMyLocation() async {
+    PermissionStatus status = await Permission.location.request();
+    if (status.isGranted) {
+      try {
+        Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
+        );
+
+        LatLng myLocation = LatLng(position.latitude, position.longitude);
+
+        mapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: myLocation,
+              zoom: _zoomLevel, // 서울에서 33배 가까이로 줌
+            ),
+          ),
+        );
+
+        _markers.add(
+          Marker(
+            markerId: MarkerId('my_location'),
+            position: myLocation,
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueBlue),
+            infoWindow: InfoWindow(
+              title: 'My Location',
+              snippet: 'Lat: ${position.latitude}, Lng: ${position.longitude}',
+            ),
+          ),
+        );
+
+        Circle circleBoundary = getCircleBoundary(myLocation, 500);
+        _circles.clear();
+        _circles.add(circleBoundary);
+        print(
+            'Circle Center: ${circleBoundary.center}, Radius: ${circleBoundary.radius}');
+
+        // 위치 정보 프린트
+        print('My Location: Lat: ${position.latitude}, Lng: ${position.longitude}');
+
+        setState(() {});
+      } catch (e) {
+        print('Error: $e');
+      }
+    } else {
+      print('User denied permissions to access the device\'s location.');
+    }
+  }
+
+  void _showMarkersAroundSelectedLocation() async {
+    if (_selectedLocation != null) {
+      List<Place> places = (await getData()).cast<Place>();
+      _markers.clear();
+      _circles.clear();
+
+      _circles.add(getCircleBoundary(
+          _selectedLocation!, 600));
+
+      LatLng circleCenter = _selectedLocation!;
+      double circleRadius = 600;
+
+      for (var place in places) {
+        LatLng? location = await place.location;
+        if (location != null) {
+          double distance = Geolocator.distanceBetween(
+            circleCenter.latitude,
+            circleCenter.longitude,
+            location.latitude,
+            location.longitude,
+          );
+
+          if (distance <= circleRadius) {
+            _markers.add(
+              Marker(
+                markerId: MarkerId(place.name),
+                position: location,
+                infoWindow: InfoWindow(
+                  title: place.name,
+                  snippet: place.address,
+                ),
+              ),
+            );
+          }
+        }
+      }
+
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: _zoomLevel,
+            ),
+            markers: _markers,
+            circles: _circles,
+          ),
+          Positioned(
+            top: 50.0,
+            left: 0.0,
+            right: 20.0,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {
+                        // Handle back button press
+                        Navigator.pop(context);
+                      },
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.white,
+                        child: TextField(
+                          onChanged: _filterPlaces,
+                          decoration: InputDecoration(
+                            hintText: '장소 검색...',
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.all(8.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5.0),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Row(
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _selectedCategory = '한식';
+                            });
+                          },
+                          child: Text(
+                            '한식',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              side: BorderSide(color: Colors.grey),
+                            ),
+                            elevation: 5,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _selectedCategory = '중식';
+                            });
+                          },
+                          child: Text(
+                            '중식',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              side: BorderSide(color: Colors.grey),
+                            ),
+                            elevation: 5,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _selectedCategory = '일식';
+                            });
+                          },
+                          child: Text(
+                            '일식',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              side: BorderSide(color: Colors.grey),
+                            ),
+                            elevation: 5,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _selectedCategory = '카페';
+                            });
+                          },
+                          child: Text(
+                            '카페',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              side: BorderSide(color: Colors.grey),
+                            ),
+                            elevation: 5,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _selectedCategory = '포장마차';
+                            });
+                          },
+                          child: Text(
+                            '포장마차',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              side: BorderSide(color: Colors.grey),
+                            ),
+                            elevation: 5,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getCurrentMyLocation,
+        child: Icon(Icons.my_location),
+        backgroundColor: Colors.blue[700],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+    );
+  }
+}
